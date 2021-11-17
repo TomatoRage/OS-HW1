f04580ae4328056ff3cbc203193329dad7eb7600
TomatoRage <61627067+TomatoRage@users.noreply.github.com>
2021-11-17 14:10:37 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/HW_OS1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_OS1.dir/Commands.cpp.obj"
+  "CMakeFiles/HW_OS1.dir/signals.cpp.obj"
+  "CMakeFiles/HW_OS1.dir/smash.cpp.obj"
+  "HW_OS1.exe"
+  "HW_OS1.exe.manifest"
+  "HW_OS1.pdb"
+  "libHW_OS1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_OS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
