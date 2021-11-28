9d29d1780f3171a972070d8455dd20f9e045fabf
בשאר עומרי <bashar_omari@hotmail.com>
2021-11-28 14:32:58 +0200

    signals ctrlC finished

--- signals.cpp
+++ signals.cpp
@@ -11,26 +11,27 @@ void ctrlZHandler(int sig_num) {
     cout << "smash: got ctrl-Z" << endl;
     if (SmallShell::getInstance().currCommand == NULL) return;
     int proID = SmallShell::getInstance().currCommand->processPID;
-    if (SmallShell::getInstance().currCommand->Type == PIPE ||
-        SmallShell::getInstance().currCommand->Type == REDIRECTION ||
-        SmallShell::getInstance().currCommand->Type == PIPE) {//time o
-        if (killpg(proID, SIGSTOP) == -1) {
-            perror("smash error: kill failed");
-            return;
-        }
-    } else if (kill(proID, SIGSTOP) == -1) {
+    if (kill(proID, SIGSTOP) == -1) {
         perror("smash error: kill failed");
         return;
     }
     cout << "smash: process " << proID << " was stopped" << endl;
-    //SmallShell::getInstance().currCommand->isFinished = true;
-        SmallShell::getInstance().jobList->addJob(SmallShell::getInstance().currCommand, true);
-        SmallShell::getInstance().currCommand = NULL;
+    SmallShell::getInstance().jobList->addJob(SmallShell::getInstance().currCommand, true);
+    SmallShell::getInstance().currCommand = NULL;
 }
 
 void ctrlCHandler(int sig_num) {
-    // TODO: Add your implementation
-
+    cout << "smash: got ctrl-C" << endl;
+    if (SmallShell::getInstance().currCommand == NULL) return;
+    int proID = SmallShell::getInstance().currCommand->processPID;
+    if (kill(proID, SIGKILL) == -1) {
+        perror("smash error: kill failed");
+        return;
+    }
+    cout << "smash: process " << proID << " was killed" << endl;
+    int job = SmallShell::getInstance().jobList->getJobByPID(proID)->jobID;
+    SmallShell::getInstance().jobList->removeJobById(job);
+    SmallShell::getInstance().currCommand = NULL;
 }
 
 void alarmHandler(int sig_num) {
